031e286f8191b6b8146ed7afe0eb47a1a7c0d9e0
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-01-12 14:54:49 +0100

    Taking ROOT from fcc-install area.

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/2.8.9/Linux-i386/bin:${PATH}
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.9.3/x86_64-slc6/setup.sh
-source /afs/cern.ch/sw/lcg/releases/ROOT/6.04.06-458fa/x86_64-slc6-gcc49-opt/bin/thisroot.sh
+source /afs/cern.ch/exp/fcc/sw/0.5/LCG_80/ROOT/6.04.06/x86_64-slc6-gcc49-opt/bin/thisroot.sh
 
 export ALBERS=$PWD/install
 export LD_LIBRARY_PATH=$ALBERS/examples:$ALBERS/lib:$LD_LIBRARY_PATH
